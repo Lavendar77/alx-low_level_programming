0edef73c3e0f87593701f2b263e7e7b4419cdb89
Adeyinka Adefolurin <folurinyinka@gmail.com>
2023-02-21 11:42:29 -0800

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - absolute value
+ * @n: number to check
+ *
+ * Description: get absolute value of number
+ *
+ * Return: int
+*/
+int _abs(int n)
+{
+	return (abs(n));
+}
