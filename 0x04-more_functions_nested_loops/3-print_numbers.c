b344a3d7d9306a3a7821e1d0ba7785f94cf90e46
Adeyinka Adefolurin <folurinyinka@gmail.com>
2023-02-26 07:09:35 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers
+ *
+ * Return: void
+*/
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		_putchar('0' + i);
+		i++;
+	}
+
+	_putchar('\n');
+}
