ef5fa104cddcf37642081754abcbe8ba7aa918e6
Adeyinka Adefolurin <folurinyinka@gmail.com>
2023-02-26 07:18:14 -0800

    print numbers except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print most numbers
+ *
+ * Description: print numbers except 2 and 4
+ *
+ * Return: void
+*/
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar('0' + i);
+		}
+
+		i++;
+	}
+
+	_putchar('\n');
+}
