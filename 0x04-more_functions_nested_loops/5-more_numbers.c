ecb343e39ba8786820da1445921fe52f3b51b0a7
Adeyinka Adefolurin <folurinyinka@gmail.com>
2023-02-26 07:32:08 -0800

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * more_numbers - more numbers
+ *
+ * Description: print 10 rows of numbers from 0 to 14
+ *
+ * Return: void
+*/
+void more_numbers(void)
+{
+	int i = 0;
+
+	for (i = 0; i <= 10; i++)
+	{
+		int j = 0;
+
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+			{
+				_putchar((j / 10) + '0');
+			}
+
+			_putchar((j % 10) + '0');
+		}
+
+		_putchar('\n');
+	}
+}
