ae38b8815662f45544abdf5560b12e018d2668e7
Adeyinka Adefolurin <folurinyinka@gmail.com>
2023-03-10 10:32:01 -0800

    create final libmy.a

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - get string length
+ * @s: value to get length of
+ *
+ * Return: int
+*/
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
