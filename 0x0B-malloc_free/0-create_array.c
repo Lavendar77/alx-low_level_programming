c3e363dff9a3af98b03b56b542bb8b9386da6136
Adeyinka Adefolurin <folurinyinka@gmail.com>
2023-03-14 11:21:52 -0700

    fix issue malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@
 */
 char *create_array(unsigned int size, char c)
 {
-	char *array = malloc(sizeof(unsigned int) * size);
+	char *array = malloc(sizeof(char) * size);
 	unsigned int i = 0;
 
 	if (size == 0)
