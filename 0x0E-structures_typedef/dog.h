6ac96ebef0f7b8a6df531fea1d7ff209db8fc4c1
Adeyinka Adefolurin <folurinyinka@gmail.com>
2023-03-20 15:24:19 -0700

    define a dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog object
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: ownder of the dog
+*/
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
+typedef struct dog dog_t;
+
+#endif /* DOG_H */
